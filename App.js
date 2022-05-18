9af5f4da41d4aa793ea1c4dbfc9f75a3e5f6b04d
mollyjs823 <mollyjs@hawaii.edu>
2022-05-17 21:47:48 -0600

    Add item implemented

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, {useState} from "react";
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList, Alert } from 'react-native';
 import uuid from "react-native-uuid";
 
 import Header from "./components/Header";
 import ListItem from "./components/ListItem";
+import AddItem from "./components/AddItem";
 
 const App = () => {
   const [items, setItems] = useState([
@@ -29,17 +30,29 @@ const App = () => {
     setItems(prevItems => {
       return prevItems.filter(item => item.id != id);
     });
-  }
+  };
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', [{ text: 'OK' }]);
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid.v4(), text}, ...prevItems];
+      });
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
+      <AddItem addItem = {addItem}/>
       <FlatList
         data={items}
         renderItem={({item}) => (
           <ListItem
             item={item}
             deleteItem = {deleteItem}
+            addItem = {addItem}
           />
         )}
       />
--- components/ListItem.js
+++ components/ListItem.js
@@ -31,5 +31,3 @@ const styles = StyleSheet.create({
 });
 
 export default ListItem;
-
-//27 min into tutorial
